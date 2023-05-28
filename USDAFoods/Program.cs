7ff200bc92d1d845c411cc5fed429db323cb0f2b
Walt Beaman <walter.g.beaman@outlook.com>
2023-05-27 21:28:28 -0500

    Initial commit.

---
+++ USDAFoods/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace USDAFoods
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            Console.Write("Enter a food item to search: ");
+            string query = Console.ReadLine();
+
+            UsdaFoodApiClient client = new UsdaFoodApiClient(new HttpClient(), config);
+            string searchResultJson = await client.SearchFood(query);
+
+            // Parse the JSON response
+            var searchResult = JsonSerializer.Deserialize<JsonDocument>(searchResultJson);
+            var foods = searchResult.RootElement.GetProperty("list").GetProperty("item").EnumerateArray();
+
+            if (!foods.Any())
+            {
+                Console.WriteLine("No results found.");
+            }
+            else
+            {
+                Console.WriteLine("Search Results:");
+                foreach (var food in foods)
+                {
+                    string name = food.GetProperty("name").GetString();
+                    string ndbno = food.GetProperty("ndbno").GetString();
+                    Console.WriteLine($"- {name} (NDB Number: {ndbno})");
+                }
+            }
+        }
+    }
+}
---
+++ USDAFoods/UsdaFoodApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+
+namespace USDAFoods
+{
+    internal class UsdaFoodApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public UsdaFoodApiClient(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<string> SearchFood(string query)
+        {
+            string apiKey = _config["UsdaApiKey"];
+            string url = $"https://api.nal.usda.gov/fdc/v1/foods/search?api_key={apiKey}&query={query}";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
